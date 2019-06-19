292b1ecde500581359cbb782804827e7ee1014fc
Taylor Warren <taylorWarrenCoding@gmail.com>
2019-06-18 18:10:21 -0600

    making Game to Games for easier readability

--- app/Controllers/Http/GameController.js
+++ app/Controllers/Http/GameController.js
@@ -1,36 +1,33 @@
 'use strict'
-const Game = use('App/Models/Game')
+const Games = use('App/Models/Game')
 class GameController {
   async index({response}) {
-    const games = await Game
-    .query()
-    .with('developer')
-    .fetch();
+    const games = await Games.all()
     response.send(games)
   }
   async store({request, response}) {
-    const game = request.only(['name', 'rating', 'developer_id'])
-    await Game.create(game)
+    const game = request.only(['name', 'rating'])
+    await Games.create(game)
     const games = await Games.all()
     response.send(games)
   }
   async show({request, response}) {
-    const game = await Game.find(request.params.id)
+    const game = await Games.find(request.params.id)
     response.send(game)
   }
   async update({request, response}) {
     const gameEdit = request.post()
-    const game = await Game.find(request.params.id)
+    const game = await Games.find(request.params.id)
     game.name = gameEdit.name;
     game.rating = gameEdit.rating;
     await game.save()
-    const games = await Game.all()
+    const games = await Games.all()
     response.send(games)
   }
   async destroy({request, response}) {
-    const game = await Game.find(request.params.id)
+    const game = await Games.find(request.params.id)
     await game.delete()
-    const games = await Game .all()
+    const games = await Games.all()
     response.send(games)
   }
 }
